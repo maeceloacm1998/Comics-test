1202c4e5ddab04af0f1f882455437e9fe481f50f
Marcelo Antonio <maeceloacm1998@gmail.com>
2025-02-19 01:16:35 -0300

    feat: add empty subtitle in ComicsItemHolder

--- app/src/main/java/com/example/comics/ui/holders/ComicsItemHolder.kt
+++ app/src/main/java/com/example/comics/ui/holders/ComicsItemHolder.kt
@@ -8,10 +8,12 @@ import com.example.comics.databinding.ItemListHolderBinding
 class ComicsItemHolder(
     private val binding: ItemListHolderBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
+    private val emptySubtitle = "Sem descrição"
+
     fun bind(data: ComicsItemModel) {
         binding.apply {
             actionTitle.text = data.title
-            actionSubTitle.text = data.subtitle
+            actionSubTitle.text = data.subtitle.ifEmpty { emptySubtitle }
             Glide.with(binding.root)
                 .load(data.image)
                 .centerCrop()
